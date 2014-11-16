deaaf0efdbe111d545eb29f37704df601ee756eb
cryptonate <Nathanabramson@gmail.com>
2014-11-16 17:38:01 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
+##R programming project 2 
+##Solution sourced from https://github.com/edj-boston/coursera-r-programming/blob/master/programming-assignment-2/cachematrix.R
 
-## Write a short comment describing this function
+## Two functions to cache inverse of a matrix
+## Initiated inversion matrix
+makeCacheMatrix <- function( m = matrix() ) {
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+##Initialize
+     i <- NULL
+     
+##Method to set the matrix
+     set <- function( matrix ) {
+         m <<- matrix
+         i <<- NULL
+     }
+     
+## Method to get and return the matrix
+     get <- function() {
+         m
+     }
+     
+## Sets its inverse
+     setInverse <- function(inverse) {
+         i <<- inverse
+     }
+     
+## Gets the inverse, returning the inverse property
+     getInverse <- function() {
+         i
+    }
+    
+##Returns a list of methods
+     list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+ }
+ 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
+## retrieves the inverse from the cache.
+ cacheSolve <- function(x, ...) {
+ 
+ ##Return an inverse matrix
+     m <- x$getInverse()
+     
+##Only returns if it is already set (not null)
+     if(!is.null(m)) {
+         message("getting cached data")
+         return(m)
+     }
+     
+## Get the matrix from the object
+     data <- x$get()
+     
+## Matrix multiplication to get its inverse, which is set to the object and returned
+     m <- solve(data) %*% data
+     x$setInverse(m)
+     m
+ }
