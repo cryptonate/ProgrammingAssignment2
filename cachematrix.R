b17898741a1df1dda28fefd7311f9653a95b8a3c
cryptonate <Nathanabramson@gmail.com>
2014-11-23 14:48:11 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,43 @@
 ##R programming project 2 
-##Solution sourced from https://github.com/edj-boston/coursera-r-programming/blob/master/programming-assignment-2/cachematrix.R
+##Solution sourced primarily from https://github.com/edj-boston/coursera-r-programming/blob/master/programming-assignment-2/cachematrix.R
 
 ## Two functions to cache inverse of a matrix
 ## Initiated inversion matrix
-makeCacheMatrix <- function( matr = matrix() ) {
+makeCacheMatrix <- function(matr = matrix()) {
 
 ##Initialize and set the matrix
      inv <- NULL
-     set <- function( matrix ) {
+     setmethod <- function(matrix) {
          matr <<- matrix
          inv <<- NULL
      }
      
 ## Method to get and return the matrix
-     get <- function() {
+     getmethod <- function() {
          matr
      }
      
 ## Sets the matrix's inverse
-     setInverse <- function(inverse) {
+     setInv <- function(inverse) {
          inv <<- inverse
      }
      
 ## Gets the and returns the inverse and its property
-     getInverse <- function() {
+     getInv <- function() {
          inv
     }
     
 ##Returns a list of methods
-     list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+     list(getmethod = getmethod, setmethod = setmethod, getInv = getInv, setInv = setInv)
  }
  
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## This function computes the inverse of the list of methods ("matrix") returned by makeCacheMatrix above. 
 ## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve 
-## retrieves the inverse from the cache.
+## retrieves the inverse from the cache with the line "if(!is.null(matr))".
  cacheSolve <- function(x, ...) {
  
  ##Return an inverse matrix
-     matr <- x$getInverse()
+     matr <- matr$getInv()
      
 ##Only returns if it is already set (not null)
      if(!is.null(matr)) {
@@ -46,10 +46,10 @@ makeCacheMatrix <- function( matr = matrix() ) {
      }
      
 ## Get the matrix from the object
-     data <- x$get()
+     input <- matr$get()
      
 ## Matrix multiplication to get its inverse, which is set to the object and returned
-     matr <- solve(data) %*% data
-     x$setInverse(matr)
+     matr <- solve(input) %*% input
+     matr$setInv(matr)
      matr
  }
